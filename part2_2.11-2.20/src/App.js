d3d0a65be1df3172e8ead40355ac9351d4b04800
JoonasEskonheimo <88729037+JoonasEskonheimo@users.noreply.github.com>
2023-08-03 00:19:47 +0300

    part2, exercise 2.16-2.17

--- part2_2.11-2.20/src/App.js
+++ part2_2.11-2.20/src/App.js
@@ -4,13 +4,19 @@ import Search from './components/search'
 import PersonForm from './components/personForm'
 import Header from './components/header'
 import phonebookService from './services/phonebookService'
+import SuccessNotification from './components/notifications/successNotification'
+import FailureNotification from './components/notifications/failureNotification'
 
 const App = () => {
+  const DEFAUT_NOTIFICATION_VALUE = ''
 
   const [persons, setPersons] = useState([])
   const [filteredPersons, setFilteredPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [successNotificationMessage, setNewSuccessNotificationMessage] = useState(DEFAUT_NOTIFICATION_VALUE)
+  const [failureNotificationMessage, setNewFailureNotificationMessage] = useState(DEFAUT_NOTIFICATION_VALUE)
+
 
   useEffect(() => {
     fetchPersons()
@@ -19,28 +25,71 @@ const App = () => {
   const fetchPersons = () => {
     phonebookService.getAll().then(personsData => setPersons(personsData))
   }
+  const resetNotifications = () => {
+    setNewFailureNotificationMessage(DEFAUT_NOTIFICATION_VALUE)
+    setNewSuccessNotificationMessage(DEFAUT_NOTIFICATION_VALUE)
+  }
 
   useEffect(() => {
     setFilteredPersons(persons);
   }, [persons])
 
+  useEffect(() => {
+    if (successNotificationMessage !== DEFAUT_NOTIFICATION_VALUE) {
+      setTimeout(() => setNewSuccessNotificationMessage(DEFAUT_NOTIFICATION_VALUE), 3000)
+    }
+  }, [successNotificationMessage])
+
   const personFormSubmitHandler = (event) => {
     event.preventDefault()
+    resetNotifications()
+  
+    const existingPerson = findPersonByName(newName)
+    const nameObj = { name: newName, number: newNumber }
+    const update = existingPerson && userWantsUpdate(existingPerson.name)
+    const create = !existingPerson
 
-    const existingPerson = persons.find(person => person.name === newName)
-    const nameObj = {
-      name: newName,
-      number: newNumber
-    }
-    const userWantsUpdate = () => {
-      return window.confirm(`${newName} already exists in phonebook, do you want to update it?`)
-    }
-    if (existingPerson && userWantsUpdate()) {
-      phonebookService.update(existingPerson.id, nameObj).then(updatedPerson => setPersons(persons.map(person => person.id === updatedPerson.id ? updatedPerson : person)))
-      return;
+    if (create) {
+      createPerson(nameObj)
+        .then(addedPerson => addPersonToPhonebook(addedPerson))
+        .catch(error => { setNewFailureNotificationMessage(`Failed to add ${newName}`) })
     }
 
-    phonebookService.create(nameObj).then(resp => setPersons(persons.concat(resp)))
+    if (update) {
+      updatePerson(existingPerson.id, nameObj)
+        .then(updatedPerson => updatePersonInPhonebook(updatedPerson))
+        .catch(error => setNewFailureNotificationMessage(`Failed to update ${newName}`))
+    }
+  }
+
+  const findPersonByName = (name) => {
+    return persons.find(person => person.name === name);
+  }
+
+  const userWantsUpdate = (name) => {
+    return window.confirm(`${name} already exists in the phonebook, do you want to update it?`);
+  }
+
+  const updatePerson = (id, nameObj) => {
+    return phonebookService.update(id, nameObj);
+  }
+
+  const updatePersonInPhonebook = (updatedPerson) => {
+    setPersons(persons.map(person => person.id === updatedPerson.id ? updatedPerson : person));
+    setNewSuccessNotificationMessage(`Updated ${updatedPerson.name}`);
+  }
+
+  const handleUpdateError = (name) => {
+    setNewFailureNotificationMessage(`Failed to update ${name}`);
+  }
+
+  const createPerson = (nameObj) => {
+    return phonebookService.create(nameObj);
+  }
+
+  const addPersonToPhonebook = (addedPerson) => {
+    setPersons(persons.concat(addedPerson))
+    setNewSuccessNotificationMessage(`Added ${addedPerson.name}`);
   }
 
   const handleNameChange = (event) => {
@@ -63,7 +112,12 @@ const App = () => {
     newNumber: newNumber
   }
   const deleteHandler = (id) => {
-    phonebookService.remove(id).then(response => setPersons(persons.filter(person => person.id !== id)))
+    phonebookService.remove(id)
+      .then(response => setPersons(persons.filter(person => person.id !== id)))
+      .catch(error => {
+        const failedPerson = persons.find(person => person.id === id)
+        setNewFailureNotificationMessage(`Failed to remove ${failedPerson.name}. It might be already removed.`)
+      })
   }
   const phonebookNumbersHandlers = {
     deleteHandler: deleteHandler
@@ -72,11 +126,13 @@ const App = () => {
   return (
     <div>
       <Header text='Phonebook' />
+      <SuccessNotification message={successNotificationMessage} />
+      <FailureNotification message={failureNotificationMessage} />
       <Search onChangeHandler={searchChangeHandler} />
       <PersonForm handlers={personFormHandlers} personFormState={personFormState} />
       <PhonebookNumbers handlers={phonebookNumbersHandlers} numbers={filteredPersons} />
     </div>
   )
 }
-
+{ }
 export default App
---
+++ part2_2.11-2.20/src/components/notifications/failureNotification.js
@@ -0,0 +1,8 @@
+const FailureNotification = ({ message }) => {
+    return (
+        <div className="failureNotification">
+            {message}
+        </div>
+    )
+}
+export default FailureNotification
